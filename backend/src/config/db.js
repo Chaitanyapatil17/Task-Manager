06709c617a0ff5125c3e7dadb305c6979acdc89d
Chaitanya Patil <patilchaitanya974@gmail.com>
2026-05-03 17:11:44 +0530

    fix crypto error

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import crypto from "crypto";
+
+global.crypto = crypto;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 };
